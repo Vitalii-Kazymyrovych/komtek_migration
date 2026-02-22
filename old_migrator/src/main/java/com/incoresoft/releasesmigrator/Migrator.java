6079f0e636668ee9a74b8c1d46b716a11101b5db
Vitalii Kazymyrovych <vitalii.kazymyrovych@gmail.com>
2026-02-22 19:15:14 +0200

    erased

--- old_migrator/src/main/java/com/incoresoft/releasesmigrator/Migrator.java
+++
@@ -1,5 +0,0 @@
-package com.incoresoft.releasesmigrator;
-
-public interface Migrator {
-    void migrate();
-}
---
+++ src/test/java/com/incoresoft/migrator/MigratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incoresoft.migrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
